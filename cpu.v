2c1e4dbb5ca6398e1a43b0972edddb530953ad33
ujnoa <leofint@gmail.com>
2024-08-17 17:13:37 +0200

    finished build

--- cpu.v
+++ cpu.v
@@ -1,10 +1,9 @@
-module cpu(input clk, output  output_register;);
+module cpu(input clk, output reg[7:0] output_register);
 
-localparam STATE_FETCH = 0;
-localparam STATE_MEM = 1;
-localparam STATE_DECODE = 2;
-localparam STATE_EXECUTE = 3;
-localparam STATE_WRITE_BACK = 4;
+parameter LDA = 4'b0001;
+parameter ADD = 4'b0010;
+parameter OUT = 4'b0011;
+
 reg pc_in;
 reg pc_out;
 reg pc_add;
@@ -20,13 +19,39 @@ reg b_out;
 reg alu_out;
 reg output_in;
 
-reg state[4:0];
+//Clock
+reg[31:0] clk_counter;
 always @(posedge clk) begin
-    if (state == 10) begin
-        state <= 0;
-    else 
-        state <= state + 1;
+
+    clk_counter <= clk_counter + 1;
+
+    if (clk_counter > 13500000)  begin
+        
+        clk_counter <= 0;
     end
+
+
+    if (clk_counter < 7000000) begin
+        cpu_clk <= 1;
+    end
+    else begin
+        cpu_clk <= 0;
+    end
+
+    
+end
+
+
+// Instruction Step Counter
+reg[5:0] step;
+always @(posedge cpu_clk) begin
+        step <= step + 1;
+
+    if (step == 8) begin
+        step <= 0;   
+    end
+
+
 end
 
 
@@ -35,15 +60,16 @@ end
 wire[7:0] bus;
 assign bus = 
     pc_out ? pc :
-    ram_out ? ram :
-    ir_out ? ir :
+    ram_out ? ram[mar] :
+    ir_out ? ir[3:0] : //// ?
     a_out ? a :
     b_out ? b :
     alu_out ? alu :
     8'h00;
 
 // Programm Counter
-always @(posedge clk) begin
+reg[7:0] pc =0;
+always @(posedge cpu_clk) begin
     if (pc_add) begin
         pc <= pc + 1;
     end
@@ -51,17 +77,17 @@ always @(posedge clk) begin
 end
 
 // Memory Adress Register
-reg[7:0]
-always @(posedge clk) begin
+reg[7:0] mar;
+always @(posedge cpu_clk) begin
     if (mar_in) begin
-        mar <= bus;      
+        mar <= bus[3:0];      
     end
     
 end
 
 //RAM
 reg[7:0] ram[16];
-always @(posedge clk) begin
+always @(posedge cpu_clk) begin
     if (ram_in) begin
         ram[mar] <= bus;
     end
@@ -69,24 +95,141 @@ end
 
 //Instruction Register
 reg[7:0] ir;
-always @(posedge clk) begin
+always @(posedge cpu_clk) begin
     if (ir_in) begin
         ir <= bus;
     end
 end
 
-// ...... WEITERE KOMPONENTEN
 
-//Control Unit
-always @(posedge clk) begin
-    if (state == STATE_FETCH) begin
-        pc_out <= 1;
-        mar_in <= 1;
+//Output Register
+always @(posedge cpu_clk) begin
+    if (output_in) begin
+        output_register <= bus;
     end
-    if (state == ) begin
-        
-    end
+    
 end
 
+//A Register
+reg[7:0] a_reg;
+always @(posedge cpu_clk) begin
+    if (a_in) begin
+        a_reg <= bus;
+    end
+    
+end
+//B Register
+reg[7:0] b_reg;
+always @(posedge cpu_clk) begin
+    if (b_in) begin
+        b_reg <= bus;
+    end
+    
+end
+
+//ALU
+reg[7:0] alu;
+always @(posedge cpu_clk) begin
+    if (alu_out) begin
+        alu <= a_reg + b_reg;
+    end
+    
+end
+
+// ...... WEITERE KOMPONENTEN
+
+//FETCH STAGE
+always @(negedge cpu_clk) begin
+    case (step)
+        1: begin
+          pc_out <= 1;
+          mar_in <= 1;
+          ram_in <= 1;
+        end
+        2: begin
+          ram_out <= 1;
+          ir_in <= 1;
+          pc_add <= 1;
+        end
+        //default: 
+    endcase
+end
+
+//Control Unit
+always @(negedge cpu_clk) begin
+    if (ir[7:4] == ADD) begin
+        case (step)
+            3: begin
+              ir_out <= 1;
+              mar_in <= 1;
+              ram_in <= 1;
+            end
+            4: begin
+              ram_out <= 1;
+              b_in <= 1;
+            end
+            5: begin
+              alu_out <= 1;
+              a_in <= 1;
+            end
+            //default: 
+        endcase
+        
+    end
+    if (it[7:4] == LDA) begin
+        case (step)
+            3: begin
+              ir_out <= 1;
+              mar_in <= 1;
+              ram_in <= 1;
+            end
+            4: begin
+              ram_out <= 1;
+              a_in <= 1;
+            end 
+            //default: 
+        endcase
+        
+    end
+    if (ir[7:4] == OUT) begin
+        case (step)
+            3: begin
+                a_out <= 1;
+                output_in <= 1;
+            end 
+            //default: 
+        endcase
+        
+    end
+    
+    
+end
+initial begin
+    
+
+mem[0] = {LDA, 4'hF};
+mem[1] = {OUT, 4'h0}; 
+mem[2] = {ADD, 4'hF}; 
+mem[3] = {OUT, 4'h0}; 
+mem[4] = {ADD, 4'hF}; 
+mem[5] = {OUT, 4'h0}; 
+mem[6] = {ADD, 4'hF}; 
+mem[7] = {OUT, 4'h0}; 
+mem[8] = {ADD, 4'hF}; 
+mem[9] = {OUT, 4'h0}; 
+mem[10] = {ADD, 4'hF}; 
+mem[11] = {OUT, 4'h0};
+mem[12] = {ADD, 4'hF};
+mem[13] = {OUT, 4'h0};
+mem[14] = {ADD, 4'hF};
+mem[15] = 8'b00000001;
+
+end
+
+endmodule
+
+module top(input clk, output reg [7:0] output_register);
+
+    cpu test(clk, output_register);
 
 endmodule
